05e3c73064676ef903576389622fa972808b3d64
nikhildwivedi250-prog <nikhildwivedi250@gmail.com>
2026-05-09 07:57:30 +0530

    chiiii

---
+++ pattern/pattern_01/par16_sol.py
@@ -0,0 +1,21 @@
+for i in range(9, 0, -1):
+    for j in range(i):
+        print("34", end="")
+    print()
+for i in range(1, 9):
+    for j in range(i):
+        print("34", end="")
+    print()
+for i in range(9, 0, -1):
+    for j in range(i):
+        print("52", end="")
+    print()
+for i in range(1, 9):
+    for j in range(i):
+        print("52", end="")
+    print()
+for i in range(1, 9):
+    for j in range(1, 5):
+        print("25", end=("  "))
+    print()
+
