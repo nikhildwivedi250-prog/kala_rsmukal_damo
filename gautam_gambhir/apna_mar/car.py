8d3adbd1aea41d60a70dd9903a16580715ad4bdd
Nikhil Dwivedi <nikhildwivedi250@gmail.com>
2026-05-11 18:37:52 +0530

    time pass

---
+++ gautam_gambhir/apna_mar/car.py
@@ -0,0 +1,12 @@
+size = "medium"
+
+if size == "small":
+    print("kids")
+elif size == "medium":
+    print("young")
+elif size == "XXL":
+    print("faty man")
+elif size == "very small":
+    print("small baby")
+else:
+    print("NOT AVAILABAL IN OU STORE")
---
+++ gautam_gambhir/apna_mar/day.py
@@ -0,0 +1,18 @@
+day = "lallan top"
+
+if day == "monday":
+    print("wear green shirt")
+elif day == "tusday":
+    print("wear blue shirt")
+elif day == "wednesday":
+    print("wear pink shirt")
+elif day == "thursday":
+    print("wear purpal shirt")
+elif day == "friday":
+    print("wear white shirt")
+elif day == "saturday":
+    print("wear black shirt")
+elif day == "sunday":
+    print("wear yellow shirt")
+else:
+    print(" Please enter the correct name of Day!")
